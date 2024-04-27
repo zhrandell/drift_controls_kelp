4ee87dd395917d39aae1e83990a299ebc4245129
Zachary Randell <z.randell@seattleaquarium.org>
2024-04-27 11:47:08 -0700

    fit stan model

---
+++ code/fit_stan_model.R
@@ -0,0 +1,74 @@
+## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ##
+## ~~~~~~~~~~~ Fit system of ODEs to empirical data in STAN ~~~~~~~~~~~~~~~~~~~~
+## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ##
+
+
+
+
+
+## start up ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ##
+rm(list = ls())
+  
+  
+## read in libraries
+library(cmdstanr)
+library(rstan)
+library(StanHeaders)
+library(shinystan)
+library(posterior)
+library(bayesplot)
+
+
+## check wd is appropriate
+getwd()
+
+
+## hardcode relative file paths
+code <- "../code"
+data_input <- "../data_input"
+data_output <- "../data_output"
+figs <- "../figs"
+
+
+## specify directory and open data
+setwd(data_output)
+
+loss_dat <- readRDS("loss_dat.Rdata")
+## END start up ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+
+
+
+## run Stan ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+## load directory with Stan script
+setwd(code)
+
+
+## compile model
+model <- cmdstan_model(file <- "restocking_stan_model.stan")
+
+
+## initiate sampling 
+fit <- model$sample(data = Loss.Dat, 
+                  chains = 1,
+                  iter_warmup = 1000,
+                  iter_sampling = 2000,
+                  adapt_delta=0.80, 
+                  parallel_chains = 4) 
+
+
+# set path to Model output
+setwd(data_output)
+
+## save via cmdstan's preferred method
+fit$save_object(file="model_output_V1.RDS")
+## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+
+
+
+## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+## END of script ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

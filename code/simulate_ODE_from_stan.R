ffc5ac1ad046d3c285297764f203716aa4e580ca
Zachary Randell <z.randell@seattleaquarium.org>
2024-10-29 08:54:58 -0700

    update ODE model

--- code/simulate_ODE_from_stan.R
+++ code/simulate_ODE_from_stan.R
@@ -95,15 +95,18 @@ parm_list = c(a = aL[1],
 
 
 ## specify ODE function 
-resourceLoss <- function(S0, A0, H0, params) {
-  with(as.list(c(S0, A0, H0)),{
+resourceLoss <- function(S0, A0, F0, params) {
+  with(as.list(c(S0, A0, F0)),{
+ 
+    f_S <- S * a * (1 - (1 + exp(w + log(S / A))) / (1 + exp(log(2) + w + log(S / A)) + exp(q + 2 * log(S / A)))) 
+    f_A <- A * a * ((1 + exp(w + log(S / A))) / (1 + exp(log(2) + w + log(S / A)) + exp(q + 2 * log(S / A)))) 
+    H = exp(- v * F)
     
-    dSdt <- (-(a * S) * (1 - (1 / ((1 + S/A)^q))) * ((v - H) / v))
-    dAdt <- (-(a * A) * (1 / ((1 + S/A)^q)) * ((v - H) / v))
-    dHdt <- (((a * S) * (1 - (1 / ((1 + S/A)^q))) * ((v - H) / v)) + 
-               ((a * A) * (1 / ((1 + S/A)^q)) * ((v - H) / v)) - (p * H))
+    dS_dt = f_S * H
+    dA_dt = f_A * H
+    dF_dt = f_S + f_A
     
-    return(list(c(dSdt, dAdt, dHdt)))
+    return(list(c(dS_dt, dA_dt, dF_dt)))
   })
 }
 

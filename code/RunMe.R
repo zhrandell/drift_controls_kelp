00fd463d8b92ff730f67df4e6a76ba35dd8aa377
Mark Novak <mnovak.m@gmail.com>
2026-05-12 11:47:47 -0700

    Split out package loading

--- code/RunMe.R
+++ code/RunMe.R
@@ -9,54 +9,6 @@ setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 sel.model <- c('Logistic', 'vanLeeuwen')[1]
 
 ## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-## Libraries ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-## Libraries for 'empirical.R'
-library(tidyverse)
-library(scales)
-library(patchwork)
-
-## Libraries for 'format_data.R' 
-library(stats)
-library(gridExtra)
-library(gtable)
-library(grid)
-library(deSolve)
-library(rstudioapi)
-library(reshape2)
-library(hexbin)
-library(janitor)
-library(diffdf)
-
-## Libraries for 'fit_stan_model.R' & 'analyze_visualize.R'
-library(cmdstanr)
-library(rstan)
-library(StanHeaders)
-library(shinystan)
-library(posterior)
-library(bayesplot)
-
-## Libraries for 'analyze_visualize.R'
-library(patchwork)
-library(Cairo) # requires installation of XQuartz on machine
-library(stargazer)
-
-## Libraries for 'simulate.R' and 'plot_simulation'
-library(deSolve)
-library(reshape2)
-library(ggplot2)
-library(tidyselect)
-library(scales)
-library(pbapply)
-library(parallel)
-library(grid)
-
-## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-
-
-
-
-## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ## relative file paths
 code <- "../code"
 data <- "../data"
@@ -68,6 +20,7 @@ figs <- "../figs"
 
 
 ## source files ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+source('load_packages.R')
 source('plot_juvenile_kelp.R')
 source('empirical.R')
 source('format_data.R')
---
+++ code/load_packages.R
@@ -0,0 +1,40 @@
+## Libraries ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+## Libraries for 'empirical.R'
+library(tidyverse)
+library(scales)
+library(patchwork)
+
+## Libraries for 'format_data.R' 
+library(stats)
+library(gridExtra)
+library(gtable)
+library(grid)
+library(deSolve)
+library(rstudioapi)
+library(reshape2)
+library(hexbin)
+library(janitor)
+library(diffdf)
+
+## Libraries for 'fit_stan_model.R' & 'analyze_visualize.R'
+library(cmdstanr)
+library(rstan)
+library(StanHeaders)
+library(shinystan)
+library(posterior)
+library(bayesplot)
+
+## Libraries for 'analyze_visualize.R'
+library(patchwork)
+library(Cairo) # requires installation of XQuartz on machine
+library(stargazer)
+
+## Libraries for 'simulate.R' and 'plot_simulation'
+library(deSolve)
+library(reshape2)
+library(ggplot2)
+library(tidyselect)
+library(scales)
+library(pbapply)
+library(parallel)
+library(grid)
